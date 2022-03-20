32e0371fb714ccf319d9db522b5c8d0cc3ad1764
jongyoon.jung <okhee.okey@gmail.com>
2022-03-20 17:28:20 +0900

    [#12] temp

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
 export const MUSIC_EXTENSIONS = ['mp3', 'flac'];
+export const HTML_EXTENSIONS = ['html', 'htm'];
--- src/pathUtils.js
+++ src/pathUtils.js
@@ -1,6 +1,7 @@
 import { existsSync, mkdirSync, rmdirSync, readdirSync } from 'fs';
 import { join, extname } from 'path';
-import { MUSIC_EXTENSIONS } from './constants.js';
+import * as cheerio from 'cheerio';
+import { MUSIC_EXTENSIONS, HTML_EXTENSIONS } from './constants.js';
 
 export const mkdir = (basePath = '.') => {
     const subPathList = ['img', 'output'];
@@ -20,8 +21,8 @@ export const rmdir = (basePath = '.') => {
 
 const isMusicExists = path => {
     return readdirSync(path)
-        .map(filename => extname(filename).replace('.', ''))
-        .some(ext => MUSIC_EXTENSIONS.includes(ext.toLowerCase()));
+        .map(filename => extname(filename).replace('.', '').toLowerCase())
+        .some(ext => MUSIC_EXTENSIONS.includes(ext));
 };
 
 export const getInputDirectory = (basePath = '.') => {
@@ -31,4 +32,28 @@ export const getInputDirectory = (basePath = '.') => {
         throw new Error('No music file exists in input folder');
     }
     return musicPath;
+};
+
+const getMusicListHtml = (basePath = '.') => {
+    const subPathList = ['input', ''];
+    const htmlPath = subPathList
+        .map(p => join(basePath, p))
+        .filter(p => existsSync(p))
+        .flatMap(p => readdirSync(p))
+        .find(filename => HTML_EXTENSIONS.includes(extname(filename).replace('.', '').toLowerCase()));
+    if (!htmlPath) {
+        throw new Error('No music list html exists');
+    }
+    return htmlPath;
+};
+
+const parseMusicListHtml = musicListHtml => {
+    const $ = cheerio.load(musicListHtml);
+    return $;
+};
+
+export const getMusicList = (basePath = '.') => {
+    const musicListHtml = getMusicListHtml(basePath);
+    const musicList = parseMusicListHtml(musicListHtml);
+    return musicList;
 };
